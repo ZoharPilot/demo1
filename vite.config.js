32ec9a77245441d9319decc0bde7204babf56cb6
zohar <thezohar@gmail.com>
2025-01-28 21:14:21 +0200

    Deploy to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,9 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig(({ mode }) => {
-  // mode הוא 'development' בסביבה מקומית או 'production' בזמן build
   const isProduction = mode === 'production';
-
   return {
-    base: isProduction ? '/demo1/' : '/', // '/demo1/' לפרודקשן, '/' למקומי
+    base: isProduction ? '/demo1/' : '/', // תואם לנתיב שלך בפרודקשן
     plugins: [react()],
   };
 });
